2e2e4fd57f5398cb54ded1a19503ae322dba340d
Ionel Aipatioaei <lionel.developer72@gmail.com>
2018-07-31 12:45:02 +0300

    mobile nav finished

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function menu() {
-    alert("It works!");
+function showMenu() {
+    document.querySelector(".mobile-nav").style.transform = "translateX(0%)";
+    document.querySelector(".mobile-nav-background").style.display = "block";
+}
+
+function hideMenu() {
+    document.querySelector(".mobile-nav").style.transform = "translateX(-100%)";
+    document.querySelector(".mobile-nav-background").style.display = "none";
 }
